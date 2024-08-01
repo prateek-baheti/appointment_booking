258425ca3b0bd5dd308dd60e4fabe59c167c2e1c
prateek-baheti <prateek28baheti@gmail.com>
2024-08-01 22:49:48 +0530

    added intital setup

---
+++ appointmentservice/src/test/java/com/appointment/service/appointmentservice/AppointmentserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appointment.service.appointmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
